db989b248c5842285c95cb0b88af57fb2d5b6f05
awkwardusername <markjayson.fuentes@pup.edu.ph>
2014-01-29 20:27:20 +0800

    Syntax Errors: Are the bane of mankind

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -10,7 +10,6 @@ class BaseController extends Controller {
 	protected function setupLayout() {
 		if ( ! is_null($this->layout)) {
             $this->layout = View::make($this->layout);
-		*/
+		}
 	}
-
 }
